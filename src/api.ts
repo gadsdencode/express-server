7a7f4f7d6d30b9957d5d3bcaccee8bcd94fdae47
gadsdencode <jordan.martens@osscontact.com>
2024-02-09 06:49:32 -0500

    updated server

--- src/api.ts
+++ src/api.ts
@@ -207,3 +207,8 @@ api.post('/send-message', async (req: Request, res: Response) => {
 
 // Version the api
 app.use('/api/v1', api)});
+
+// Start the server at the very end
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server started on port ${process.env.PORT || 33333}`);
+});
